9cfa20440911be166c0372111ee987d22af7c051
Tor Lindström <tor.lindstrom.jobb@gmail.com>
2024-11-01 06:55:18 +0100

    feat: single number is returned as is

---
+++ RangeExtraction/Kata.cs
@@ -0,0 +1,9 @@
+namespace RangeExtraction;
+
+public class Kata
+{
+    public static string Extract(int[] ints)
+    {
+        return ints[0].ToString();
+    }
+}
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,10 +1,14 @@
+using RangeExtraction;
+
 namespace Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Fact(DisplayName = "One number is returned as is")]
+    public void OneNumber_IsReturnedAsIs()
     {
-
+        var result = Kata.Extract([1]);
+        
+        Assert.Equal("1", result);
     }
 }
