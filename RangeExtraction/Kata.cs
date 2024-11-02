4fe5b4586acc2bd9e94ad8bf6182345d9bb5fc58
Tor Lindström <tor.lindstrom.jobb@gmail.com>
2024-11-02 08:34:47 +0100

    refactor: improve readability
    by extracting and naming methods, and renaming variables

--- RangeExtraction/Kata.cs
+++ RangeExtraction/Kata.cs
@@ -3,14 +3,19 @@ namespace RangeExtraction;
 public class Kata
 {
     private static int[]? _ints;
-    
+
     public static string Extract(int[] ints)
     {
         _ints = ints;
         
         return _ints
-            .Select((i, idx) => i + (IsLastNumber(idx) ? "" : ChooseSeparator(i, idx)))
-            .Aggregate("", (acc, curr) => acc + curr);
+            .Select(SeparatorForEachNumber())
+            .Aggregate("", IntoAString());
+    }
+
+    private static Func<int, int, string> SeparatorForEachNumber()
+    {
+        return (number, index) => number + (IsLastNumber(index) ? "" : ChooseSeparator(number, index));
     }
 
     private static string ChooseSeparator(int i, int idx)
@@ -21,5 +26,10 @@ public class Kata
     private static bool IsLastNumber(int idx)
     {
         return idx == _ints!.Length - 1;
+    }
+
+    private static Func<string, string, string> IntoAString()
+    {
+        return (accumulate, current) => accumulate + current;
     }
 }
