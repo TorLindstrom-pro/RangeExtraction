015ca70e226a38a10c2410c885a5a99a037b61c0
Tor Lindström <tor.lindstrom.jobb@gmail.com>
2024-11-02 08:09:56 +0100

    feat: comma-separated result if two numbers

--- RangeExtraction/Kata.cs
+++ RangeExtraction/Kata.cs
@@ -4,6 +4,6 @@ public class Kata
 {
     public static string Extract(int[] ints)
     {
-        return ints[0].ToString();
+        return ints.Length == 1 ? ints[0].ToString() : ints[0] + "," + ints[1];
     }
 }
--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -11,4 +11,12 @@ public class UnitTest1
         
         Assert.Equal("1", result);
     }
+    
+    [Fact(DisplayName = "Two numbers is returned with comma separator")]
+    public void TwoNumbers_IsReturnedWithCommaSeparator()
+    {
+        var result = Kata.Extract([1,3]);
+        
+        Assert.Equal("1,3", result);
+    }
 }
